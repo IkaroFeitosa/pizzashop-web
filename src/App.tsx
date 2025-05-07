f783a9b73bc3474bdd71e300a73f040c2e6be605
Ikaro Feitosa <ikaro.feitosa.ext@espm.br>
2025-05-06 22:07:01 -0300

    chore: Instalando shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import "@/global.css";
+import { Button } from "./components/ui/button";
 
 export function App() {
-
   return (
-   <h1>Hello word!</h1>
-  )
+    <div className="flex items-center justify-center h-screen flex-col">
+      <h1>Hello word!</h1>
+      <Button>teste</Button>
+    </div>
+  );
 }
-
