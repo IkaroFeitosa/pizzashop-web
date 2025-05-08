56b83388ece3d01b33a5d8e495a88c5f242db406
Ikaro Feitosa <ikaro.feitosa.ext@espm.br>
2025-05-07 22:46:57 -0300

    feat: add react-hook-form

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,8 +1,28 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const signInForm = z.object({
+  email: z.string().email(),
+});
+
+type SignInForm = z.infer<typeof signInForm>;
 
 export function Signin() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>({
+    resolver: zodResolver(signInForm),
+  });
+  async function handleSignIn(data: SignInForm) {
+    console.log(data);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  }
   return (
     <div>
       <Helmet title="Login" />
@@ -13,15 +33,16 @@ export function Signin() {
           </h1>
           <p>Acompanhe suas vendas pelo painel do parceiro</p>
         </div>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
           <div className="space-y-2">
             <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" />
+            <Input id="email" type="email" {...register("email")} />
           </div>
           <div className="space-y-2"></div>
           <button
             type="submit"
             className="w-full bg-primary text-primary-foreground py-2 rounded-md hover:bg-primary/90 transition-colors"
+            disabled={isSubmitting}
           >
             Acessar painel
           </button>
