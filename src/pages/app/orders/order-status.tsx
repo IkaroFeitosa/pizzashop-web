2a6932dc6015e383e57dcb246f6338f71612a9d8
Ikaro Feitosa <ikaro.feitosa.ext@espm.br>
2025-05-26 12:37:38 -0300

    Test: Teste unitário Order Status

--- src/pages/app/orders/order-status.tsx
+++ src/pages/app/orders/order-status.tsx
@@ -18,16 +18,28 @@ export function OrderStatus({ status }: IOrderStatusProps) {
   return (
     <div className="flex items-center gap-2">
       {status === "pending" && (
-        <span className="h-2 w-2 rounded-full bg-gray-400" />
+        <span
+          className="h-2 w-2 rounded-full bg-slate-400"
+          data-testid="badge"
+        />
       )}
       {status === "canceled" && (
-        <span className="h-2 w-2 rounded-full bg-rose-500" />
+        <span
+          className="h-2 w-2 rounded-full bg-rose-500"
+          data-testid="badge"
+        />
       )}
       {status === "delivered" && (
-        <span className="h-2 w-2 rounded-full bg-emerald-500" />
+        <span
+          className="h-2 w-2 rounded-full bg-emerald-500"
+          data-testid="badge"
+        />
       )}
       {["delivering", "processing"].includes(status) && (
-        <span className="h-2 w-2 rounded-full bg-amber-500" />
+        <span
+          className="h-2 w-2 rounded-full bg-amber-500"
+          data-testid="badge"
+        />
       )}
       <span className="text-muted-foreground font-medium">
         {orderStatusMap[status]}
